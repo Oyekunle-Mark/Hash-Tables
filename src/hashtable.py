73808eba9bd41a4acf9067ee18174417b08f0495
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-29 13:51:07 +0100

    Implemented the djb2 hashing algorithm for the hashtable

--- src/hashtable.py
+++ src/hashtable.py
@@ -51,7 +51,7 @@ class HashTable:
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
-        return self._hash(key) % self.capacity
+        return self._hash_djb2(key) % self.capacity
 
     def insert(self, key, value):
         '''
